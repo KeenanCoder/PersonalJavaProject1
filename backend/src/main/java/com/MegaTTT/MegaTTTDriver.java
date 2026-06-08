a7b3126aa84b62311139f4d004330cf8149d77dd
KeenanCoder <santizokeenan@gmail.com>
2026-06-08 18:25:11 -0400

    first commit

---
+++ backend/src/main/java/com/MegaTTT/MegaTTTDriver.java
@@ -0,0 +1,11 @@
+//package
+
+import java.util.Scanner;
+//import others
+
+public class MegaTTTDriver {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
---
+++ backend/src/main/java/com/MegaTTT/controller/userController.java
@@ -0,0 +1,26 @@
+//package
+
+import java.util.Scanner;
+//import others
+
+public class userController {
+	
+	//FIXME: private variables
+
+	//clean constructor
+	public userController() {
+		
+	}
+	
+	public userController(/*add parameters here*/) {
+		//FIXME: this.variable
+	}
+	
+	public void setMethod() {
+		
+	}
+	
+	public void getMethod() {
+		
+	}
+}
